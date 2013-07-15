12fb4d7ebd09a52174b9e2a08f84fd55333cf8cd
Rapter2006 <rapter2006@ya.ru>
2013-07-15 14:20:10 +0700

    Немного подравил картинки

--- sibGurman/src/food/sibgurman/FragmentTwo.java
+++ sibGurman/src/food/sibgurman/FragmentTwo.java
@@ -11,14 +11,14 @@ import com.actionbarsherlock.app.SherlockListFragment;
 public class FragmentTwo extends SherlockListFragment{
 	private List<ItemList> listProducts = new ArrayList<ItemList>();
 
-@Override
+	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) 
    	{
-		listProducts.add(new ItemList("«Солнцепек» с мясом","Свинина, лук, масло, молоко", R.drawable.android_logo));
+		listProducts.add(new ItemList("«Солнцепек» с мясом","Свинина, лук, масло, молоко", R.drawable.cheese));
 		listProducts.add(new ItemList("«Солнцепек» с сыром и ветчиной", "Ветчина, яйца, лук, масло", R.drawable.cheese));
-		listProducts.add(new ItemList("«Солнцепек» со сгущенным молоком", "Сгущенка, сахар, масло, молоко", R.drawable.android_logo));
-		listProducts.add(new ItemList("«Солнцепек» с творогом", "Творог, яйца, масло, мука", R.drawable.android_logo));
-		listProducts.add(new ItemList("«Солнцепек» c мясом и рисом","Cвинина, рис, масло, мука", R.drawable.android_logo));
+		listProducts.add(new ItemList("«Солнцепек» со сгущенным молоком", "Сгущенка, сахар, масло, молоко", R.drawable.cheese));
+		listProducts.add(new ItemList("«Солнцепек» с творогом", "Творог, яйца, масло, мука", R.drawable.cheese));
+		listProducts.add(new ItemList("«Солнцепек» c мясом и рисом","Cвинина, рис, масло, мука", R.drawable.cheese));
 		MobileArrayAdapter adapter = new MobileArrayAdapter(getActivity().getBaseContext(), listProducts);
 		setListAdapter(adapter);
 		return super.onCreateView(inflater, container, savedInstanceState);
