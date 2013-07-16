6d0be35e9d117aa3c9eb184691614dfa1acffd36
Rapter2006 <rapter2006@ya.ru>
2013-07-16 11:08:50 +0700

    Добавлены отконвертированные картинки

--- sibGurman/src/food/sibgurman/FragmentThree.java
+++ sibGurman/src/food/sibgurman/FragmentThree.java
@@ -1,15 +1,13 @@
 package food.sibgurman;
 
+
 import java.util.ArrayList;
 import java.util.List;
-
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.actionbarsherlock.app.SherlockListFragment;
-//import classes.ItemList;
 
 public class FragmentThree extends SherlockListFragment{
 	private List<ItemList> listProducts = new ArrayList<ItemList>();
@@ -17,11 +15,9 @@ public class FragmentThree extends SherlockListFragment{
 @Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) 
    	{
-		listProducts.add(new ItemList("«Элитные»","Свинина, лук, масло, молоко", R.drawable.));
-		listProducts.add(new ItemList("Манты «Сибирские»", "Свинина, говядина, лук, масло", R.drawable.android_logo));
-		listProducts.add(new ItemList("«Смешарики»", "Свинина, лук, масло, молоко", R.drawable.android_logo));
-		listProducts.add(new ItemList("«Едим дома» Классические", "Cвинина, говядина, масло, мука", R.drawable.android_logo));
-		listProducts.add(new ItemList("«Едим дома» Сибирские","Cвинина, говядина, масло, мука", R.drawable.android_logo));
+		listProducts.add(new ItemList("«Борщ»","Свинина, лук, картофель, говядина", R.drawable.borsh));
+		listProducts.add(new ItemList("Грибной суп «Домашние Секреты»", "Зелень, морковь, лук, грибы", R.drawable.mashrooms));
+		listProducts.add(new ItemList("«Cолянка»", "Свинина, лук, масло, колбасы", R.drawable.sol9nka));
 		MobileArrayAdapter adapter = new MobileArrayAdapter(getActivity().getBaseContext(), listProducts);
 		setListAdapter(adapter);
 		return super.onCreateView(inflater, container, savedInstanceState);
